1a8111adfd081669888652a7d6dd079637de5d03
qiaokuahai <qiaokuahai@163.com>
2020-05-01 23:47:27 +0800

    新增： 新增项目启动shell脚本,(通过python main.py的方式)。

---
+++ scripts/py_api.sh
@@ -0,0 +1,25 @@
+# /bin/bash
+
+project_dir="/root/workspace/fdemo"
+cd ${project_dir}
+. venv/bin/activate
+echo "enter env success"
+
+start(){
+    ps -ef | grep main.py | awk '{print $2}' | xargs kill -9
+    nohup python3 main.py &
+    echo "start api via python"
+}
+
+stop(){
+    ps -ef | grep main.py | awk '{print $2}' | xargs kill -9
+    echo "stop api"
+}
+
+if [ $1 = "start" ];then
+    start
+elif [ $1 = "stop" ];then
+    stop
+else
+    echo "input param does exists!"
+fi
